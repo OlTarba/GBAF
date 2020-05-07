c7c0b4cdfb17c7c9b652a43fced240e9f9438f0b
OlTarba <61558171+OlTarba@users.noreply.github.com>
2020-05-07 09:21:16 +0200

    Fix Footer

--- connexion.php
+++ connexion.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php require_once 'include/head.php'; ?>
-    <title>GBAF | Connexion </title>
-</head>
-<body>
-    <?php include_once 'include/header.php'; ?>
+<html lang="fr">
+    <head>
+        <?php require_once 'include/head.php'; ?>
+        <title>GBAF | Connexion </title>
+    </head>
+    <body>
+        <?php include_once 'include/header.php'; ?>
 
-    <div class="card-form form connexion">
-        <h3>Connexion</h3>
-        <form action="">
-            <div>
-                <label for="pseudo">Pseudonyme : </label>
-                <input type="text" required name="pseudo">
-            </div>
-            <div>
-                <label for="password">Mot de passe : </label>
-                <input type="password" required name="password">
-            </div>
+        <div class="card-form form connexion">
+            <h3>Connexion</h3>
+            <form action="">
+                <div>
+                    <label for="pseudo">Pseudonyme : </label>
+                    <input type="text" required name="pseudo">
+                </div>
+                <div>
+                    <label for="password">Mot de passe : </label>
+                    <input type="password" required name="password">
+                </div>
 
-            <button type="submit">CONNEXION</button>
-        </form>
-        <br>
-        <p>Pas encore inscrit ? <a href="inscription.php" class="link-button">S'inscrire</a></p>
-    </div>
+                <button type="submit">CONNEXION</button>
+            </form>
+            <br>
+            <p>Pas encore inscrit ? <a href="inscription.php" class="link-button">S'inscrire</a></p>
+        </div>
 
-    <?php include_once 'include/footer.php'; ?>
-</body>
+        <div class="fixed-footer">
+            <?php include_once 'include/footer.php'; ?>
+        </div>
+    </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -1,45 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php require_once 'include/head.php'; ?>
-    <title>GBAF | Inscription </title>
-</head>
-<body>
-    <?php include_once 'include/header.php'; ?>
+<html lang="fr">
+    <head>
+        <?php require_once 'include/head.php'; ?>
+        <title>GBAF | Inscription </title>
+    </head>
+    <body>
+        <?php include_once 'include/header.php'; ?>
 
-    <div class="card-form form inscription">
-        <h3>Inscription</h3>
-        <form action="">
-            <div>
-                <label for="nom">Nom <span class="required">*</span>  : </label>
-                <input type="text" required name="nom">
-            </div>
-            <div>
-                <label for="prenom">Prénom <span class="required">*</span>  : </label>
-                <input type="text" required name="prenom">
-            </div>
-            <div>
-                <label for="pseudo">Pseudonyme <span class="required">*</span>  : </label>
-                <input type="text" required name="pseudo">
-            </div>
-            <div>
-                <label for="password">Mot de passe <span class="required">*</span>  : </label>
-                <input type="password" required name="password">
-            </div>
-            <div>
-                <label for="question">Question secrête <span class="required">*</span>  : </label>
-                <input type="text" required name="question">
-            </div>
-            <div>
-                <label for="reponse">Réponse secrête <span class="required">*</span>  : </label>
-                <input type="text" required name="reponse">
-            </div>
-            <button type="submit">VALIDER</button>
-        </form>
-        <br>
-        <p>Vous êtes déjà inscrit ? <a href="connexion.php" class="link-button">Se connecter</a></p>
-    </div>
+        <div class="card-form form inscription">
+            <h3>Inscription</h3>
+            <form action="">
+                <div>
+                    <label for="nom">Nom <span class="required">*</span>  : </label>
+                    <input type="text" required name="nom">
+                </div>
+                <div>
+                    <label for="prenom">Prénom <span class="required">*</span>  : </label>
+                    <input type="text" required name="prenom">
+                </div>
+                <div>
+                    <label for="pseudo">Pseudonyme <span class="required">*</span>  : </label>
+                    <input type="text" required name="pseudo">
+                </div>
+                <div>
+                    <label for="password">Mot de passe <span class="required">*</span>  : </label>
+                    <input type="password" required name="password">
+                </div>
+                <div>
+                    <label for="question">Question secrête <span class="required">*</span>  : </label>
+                    <input type="text" required name="question">
+                </div>
+                <div>
+                    <label for="reponse">Réponse secrête <span class="required">*</span>  : </label>
+                    <input type="text" required name="reponse">
+                </div>
+                <button type="submit">VALIDER</button>
+            </form>
+            <br>
+            <p>Vous êtes déjà inscrit ? <a href="connexion.php" class="link-button">Se connecter</a></p>
+        </div>
 
-    <?php include_once 'include/footer.php'; ?>
-</body>
+        <div class="fixed-footer">
+            <?php include_once 'include/footer.php'; ?>
+        </div>
+    </body>
 </html>
