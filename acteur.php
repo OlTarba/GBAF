f94a2288a40295a42f3e00ec9e916ab4f64afce0
ThiBerth <61558171+OlTarba@users.noreply.github.com>
2020-05-07 16:11:08 +0200

    Système de Like | Dislike
    Ajout du système de like | dislike. 1 seul vote par utilisateur et ne peut pas être changer

--- acteur.php
+++ acteur.php
@@ -17,9 +17,6 @@
     $reqPost = $db->prepare('SELECT * FROM post INNER JOIN account ON post.id_user = account.id_user WHERE post.id_acteur ');
     $reqPost->execute([$id]);
 
-    // debug($reqPost->fetch());
-
-
     $reqPostCount = $db->prepare('SELECT COUNT(*) AS postCount FROM post WHERE id_acteur = ?');
     $reqPostCount->execute([$id]);
     $postCount = $reqPostCount->fetch();
@@ -34,6 +31,18 @@
     if($postCount['postCount'] == 0 || $postUserCount['postUserCount'] == 0){
         $buttonNewComment = '<a href="add-comment.php?id='.$id.'" class="new-comment">Nouveau <br> commentaire</a>';
     }
+
+    $reqLike = $db->prepare('SELECT COUNT(*) AS countLike FROM vote WHERE id_acteur = ? AND vote = ?');
+    $reqLike->execute([$id, 1]);
+    $like = $reqLike->fetch();
+
+    $reqDislike = $db->prepare('SELECT COUNT(*) AS countDislike FROM vote WHERE id_acteur = ? AND vote = ?');
+    $reqDislike->execute([$id, 2]);
+    $dislike = $reqDislike->fetch();
+
+    $reqUserVote = $db->prepare('SELECT COUNT(*) AS countVote FROM vote WHERE id_acteur = ? AND id_user = ?');
+    $reqUserVote->execute([$id, $_SESSION['id']]);
+    $voted = $reqUserVote->fetch();
 ?>
 
 <!DOCTYPE html>
@@ -59,14 +68,25 @@
             <h5><?= $postCount['postCount'] ?> commentaires</h5>
             <div class="container-button-comment">
                 <?= $buttonNewComment ?>
-                <div class="like">
-                    <a href=""><img src="img/like.svg" class="comment-like-dislike" alt="Like"></a>
-                    5
-                </div>
-                <div class="dislike">
-                    <a href=""><img src="img/dislike.svg" class="comment-like-dislike" alt="Dislike"></a>
-                    2
-                </div>
+                <?php if($voted['countVote'] == 0){ ?>
+                    <div class="like">
+                        <a href="like-system.php?value=1&id=<?= $id ?>"><img src="img/like.svg" class="comment-like-dislike" alt="Like"></a>
+                        <?= $like['countLike'] ?>
+                    </div>
+                    <div class="dislike">
+                        <a href="like-system.php?value=2&id=<?= $id ?>"><img src="img/dislike.svg" class="comment-like-dislike" alt="Dislike"></a>
+                        <?= $dislike['countDislike'] ?>
+                    </div>
+                <?php }else {  ?>
+                    <div class="like voted">
+                        <a><img src="img/like.svg" class="comment-like-dislike" alt="Like"></a>
+                        <?= $like['countLike'] ?>
+                    </div>
+                    <div class="dislike voted">
+                        <a><img src="img/dislike.svg" class="comment-like-dislike" alt="Dislike"></a>
+                        <?= $dislike['countDislike'] ?>
+                    </div>
+                <?php } ?>
             </div>
         </div>
         <?php while($post = $reqPost->fetch()){ 
---
+++ like-system.php
@@ -0,0 +1,21 @@
+<?php 
+    session_start();
+
+    require_once 'include/database.php';
+    require_once 'include/functions.php';
+
+    if(!isset($_SESSION['connect'])){
+        header('Location: connexion.php');
+
+    }
+
+    $id     = str_secur($_GET['id']);
+    $value  = str_secur($_GET['value']);
+
+    // if($value < 3 && 0 > $value){
+        $insertValue = $db->prepare('INSERT INTO vote(id_user, id_acteur, vote) VALUES(?, ?, ?)');
+        $insertValue->execute([$_SESSION['id'], $id, $value]);
+
+        header('Location: acteur.php?id='.$id);
+        exit;
+    // }
