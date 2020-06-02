b0caa1ca0f8b5cfdc9f2e81c24778e34c5c0d38d
ThiBerth <61558171+OlTarba@users.noreply.github.com>
2020-06-02 11:09:42 +0200

    Mis à jour de la base de donnée
    Mise en page de la description des acteurs

--- base de donnée/gbaf.sql
+++ base de donnée/gbaf.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 22 mai 2020 à 22:59
+-- Généré le : mar. 02 juin 2020 à 11:07
 -- Version du serveur :  10.4.11-MariaDB
--- Version de PHP : 7.4.3
+-- Version de PHP : 7.2.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -56,10 +55,10 @@ CREATE TABLE `acteur` (
 --
 
 INSERT INTO `acteur` (`id_acteur`, `acteur`, `description`, `logo`) VALUES
-(1, 'Formation & Co', 'Formation&co est une association française présente sur tout le territoire.\r\nNous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.\r\nNotre proposition : \r\nun financement jusqu’à 30 000€ ;\r\nun suivi personnalisé et gratuit ;\r\nune lutte acharnée contre les freins sociétaux et les stéréotypes.\r\n\r\nLe financement est possible, peu importe le métier : coiffeur, banquier, éleveur de chèvres… . Nous collaborons avec des personnes talentueuses et motivées.\r\nVous n’avez pas de diplômes ? Ce n’est pas un problème pour nous ! Nos financements s’adressent à tous.', 'formation_co.png'),
-(2, 'Protect People', 'Protectpeople finance la solidarité nationale.\r\nNous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.\r\n\r\nChez Protectpeople, chacun cotise selon ses moyens et reçoit selon ses besoins.\r\nProectecpeople est ouvert à tous, sans considération d’âge ou d’état de santé.\r\nNous garantissons un accès aux soins et une retraite.\r\nChaque année, nous collectons et répartissons 300 milliards d’euros.\r\nNotre mission est double :\r\nsociale : nous garantissons la fiabilité des données sociales ;\r\néconomique : nous apportons une contribution aux activités économiques.', 'protectpeople.png'),
-(3, 'DSA France', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.\r\nNous accompagnons les entreprises dans les étapes clés de leur évolution.\r\nNotre philosophie : s’adapter à chaque entreprise.\r\nNous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises\r\n', 'Dsa_france.png'),
-(4, 'Chambre des Entrepreneurs (CDE)', 'La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. \r\nSon président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.', 'CDE.png');
+(1, 'Formation & Co', 'Formation&co est une association française présente sur tout le territoire. <br>\r\nNous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé. <br>\r\n<br>\r\nNotre proposition : <br>\r\n<br>\r\nun financement jusqu’à 30 000€ <br>\r\nun suivi personnalisé et gratuit <br>\r\nune lutte acharnée contre les freins sociétaux et les stéréotypes. <br>\r\n<br>\r\nLe financement est possible, peu importe le métier : coiffeur, banquier, éleveur de chèvres… .<br> \r\nNous collaborons avec des personnes talentueuses et motivées. \r\nVous n’avez pas de diplômes ? Ce n’est pas un problème pour nous ! <br>\r\n<br>\r\nNos financements s’adressent à tous.', 'formation_co.png'),
+(2, 'Protect People', 'Protect people finance la solidarité nationale. <br>\r\nNous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale. <br>\r\n<br>\r\nChez Protect people, chacun cotise selon ses moyens et reçoit selon ses besoins. <br>\r\nProtect people est ouvert à tous, sans considération d’âge ou d’état de santé.\r\nNous garantissons un accès aux soins et une retraite.\r\n<br>\r\n<br>\r\nChaque année, nous collectons et répartissons 300 milliards d’euros. <br>\r\n<br>\r\nNotre mission est double :\r\n<br>\r\n<br>\r\nsociale : nous garantissons la fiabilité des données sociales <br>\r\néconomique : nous apportons une contribution aux activités économiques.', 'protectpeople.png'),
+(3, 'DSA France', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales. <br>\r\nNous accompagnons les entreprises dans les étapes clés de leur évolution. <br>\r\n<br>\r\nNotre philosophie : s’adapter à chaque entreprise. <br>\r\nNous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises\r\n', 'Dsa_france.png'),
+(4, 'Chambre des Entrepreneurs (CDE)', 'La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation.<br> \r\nSon président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.', 'CDE.png');
 
 -- --------------------------------------------------------
 
@@ -126,7 +125,7 @@ ALTER TABLE `vote`
 -- AUTO_INCREMENT pour la table `account`
 --
 ALTER TABLE `account`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT pour la table `acteur`
